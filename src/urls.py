0369f65f63ecc42661fd25b1dc7f9d21d5228c34
cristian to-ong <cristiantoong@gmail.com>
2022-12-24 17:47:06 +0800

    first commit

---
+++ src/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
+
+def home(request):
+    return render(request, 'home.html')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home')
+]
